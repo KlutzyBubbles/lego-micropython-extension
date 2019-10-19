e58913eae325942a80ed8966f8dbf219c80bd8a6
Lee Tzilantonis <lee.tzilantonis@ibm.com>
2019-10-19 19:31:07 +1100

    Bump Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lego-mp-extension",
-    version="0.1.7",
+    version="0.2.0",
     author="Lee Tzilantonis",
     author_email="LTzilantonis@gmail.com",
     description="Extension for the Lego EV3 MicroPython Port",
